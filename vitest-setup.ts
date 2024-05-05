7d2b296783e235462bdf572a22efaa27bed80f7e
penpeen <ikegami1022.h@gmail.com>
2024-05-05 22:01:43 +0900

    :recycle: fix linter + formatter

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,8 +1,8 @@
-import "@testing-library/jest-dom/vitest";
-import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll } from "vitest";
+import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+import { afterAll, afterEach, beforeAll } from 'vitest';
 
-import { server } from "./mocks/node.js";
+import { server } from './mocks/node.js';
 
 beforeAll(() => {
   server.listen();
